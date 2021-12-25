86d551926ce95e6fc852df61464caa46116d4229
santistebanc <santistebanc94@gmail.com>
2021-12-24 22:17:41 -0600

    questions to js

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./Admin.css";
 import gun from "./gun";
-import questions from "./questions.json";
+import questions from "./questions";
 
 function Admin({ players, question, setCorrect, setWrong }) {
   const [inputQuestion, setInputQuestion] = useState(question);
